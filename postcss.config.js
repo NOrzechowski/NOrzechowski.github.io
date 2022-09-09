481ed1a8097367bb02e58fb2f85a96f9e551704e
Neil Orzechowski <nvorzech@gmail.com>
2022-09-09 15:37:22 +0800

    try to incl. node modules

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     plugins: [
       require('postcss-import')({
-        addModulesDirectories: ["_sass/", "node_modules"]
+        addModulesDirectories: ["_sass/", "node_modules/"]
       }),
       require('tailwindcss'),
       require('autoprefixer'),
