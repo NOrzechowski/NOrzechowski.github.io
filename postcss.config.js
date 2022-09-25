d7d9d23726def91709eb06c98270eb2679ebe99e
Neil Orzechowski <nvorzech@gmail.com>
2022-09-26 00:49:32 +0800

    tweak postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,9 @@
 module.exports = {
   parser: 'postcss-scss',
   plugins: [
-    require("postcss-import"),
+    require('postcss-import')({
+      addModulesDirectories: ["_sass", "node_modules", "assets"]
+    }),
     require("tailwindcss")("./_includes/tailwind.config.js"),
     require("autoprefixer")
   ]
