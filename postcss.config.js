90eaa056ac75814dfb0fd89bf0547fd7745f1aab
Neil Orzechowski <nvorzech@gmail.com>
2022-09-26 01:40:59 +0800

    try different way of importing

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,7 @@ module.exports = {
   parser: 'postcss-scss',
   plugins: [
     require('postcss-import')({
-      addModulesDirectories: ["_sass", "node_modules", "assets"]
+      addModulesDirectories: ["_sass", "node_modules", "assets/custom-css"]
     }),
     require("tailwindcss")("./_includes/tailwind.config.js"),
     require("autoprefixer")
