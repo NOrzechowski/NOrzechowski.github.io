96ee94b278664e61591f4f4bf39fbd0e7a97ef86
Neil Orzechowski <nvorzech@gmail.com>
2022-09-26 14:41:57 +0800

    try without minima

--- postcss.config.js
+++ postcss.config.js
@@ -2,8 +2,7 @@ module.exports = {
   parser: 'postcss-scss',
   plugins: [
     require('postcss-import')({
-      path: "./_sass",
-      addModulesDirectories: ["./_sass", "/Users/neilo/dev/git/NOrzechowski.github.io/_sass", "node_modules", "assets/custom-css"]
+      addModulesDirectories: ["node_modules", "assets/custom-css"]
     }),
     require("tailwindcss")("./_includes/tailwind.config.js"),
     require("autoprefixer")
