879dbeabe2f7f1dbd817771aab170dcd656e3645
Neil Orzechowski <nvorzech@gmail.com>
2022-10-21 17:37:04 +0800

    start tracking future content ideas

--- assets/js/components/search/routes.js
+++ assets/js/components/search/routes.js
@@ -1,3 +1,5 @@
+// Future content ideas:
+// Frisbee, music, contra dancing, teaching, culture, devops, cats, tv shows
 export const routes = [
   { name: 'About', path: '/about/' },
   { name: 'Home', path: '/', displayValue: 'Neil Orzechowski' },
