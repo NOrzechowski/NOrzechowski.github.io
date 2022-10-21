1133d1c242946dd86e91b819727b05089895f8ce
Neil Orzechowski <nvorzech@gmail.com>
2022-10-21 18:56:22 +0800

    add bungee & picture layout

--- assets/js/components/search/routes.js
+++ assets/js/components/search/routes.js
@@ -1,5 +1,6 @@
 // Future content ideas:
-// Frisbee, music, contra dancing, teaching, culture, devops, cats, tv shows
+// Frisbee, music, contra dancing, teaching, company culture, devops, cats, tv shows, industries I've worked in quiz, other mini games,
+// about this site page
 export const routes = [
   { name: 'About', path: '/about/' },
   { name: 'Home', path: '/', displayValue: 'Neil Orzechowski' },
--- bungee.markdown
+++ bungee.markdown
@@ -1,7 +1,8 @@
 ---
-layout: page
-title: Bungee
+layout: picture-feature
+title: neil_goes_bungee_jumping
 permalink: /bungee/
+img_url: /assets/images/bungee.jpg
 ---
 
-A fun picture of my recent bungee jump will be coming soon!
+A fun picture of my recent bungee jump.
