2e48679c07f7033778b9991e008ae8acb2895902
Neil Orzechowski <nvorzech@gmail.com>
2025-05-31 18:05:04 +0800

    add mid year quiz

--- assets/js/components/search/routes.js
+++ assets/js/components/search/routes.js
@@ -9,4 +9,5 @@ export const routes = [
   { name: 'Blog', path: '/blog/' },
   { name: 'Food', path: '/food/' },
   { name: 'Headshot', path: '/headshot/' }
+  { name: "MidYearQuiz", path: "https://docs.google.com/presentation/d/14-sjg6bd7sc63QzAgfebtY6ScKNTxAihkrF00ThMCF8/edit?usp=sharing"}
 ]
